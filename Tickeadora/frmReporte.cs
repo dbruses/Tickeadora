978b332f47753541264516a8e6afdc65585979d1
dbruses <dbruses@gmail.com>
2022-06-08 08:34:50 -0300

    QR Dinámico

--- Tickeadora/frmReporte.cs
+++ Tickeadora/frmReporte.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Data.SQLite;
+using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System.Drawing.Imaging;
 
 namespace Tickeadora
 {
@@ -44,12 +47,61 @@ namespace Tickeadora
             //string ctrl = ds.Tables[0].Rows[0]["Fecha"].ToString().Substring(0, 2) + ds.Tables[0].Rows[0]["Fecha"].ToString().Substring(3, 2) + ds.Tables[0].Rows[0]["Fecha"].ToString().Substring(8, 2) + " " + ds.Tables[0].Rows[0]["Hora"].ToString().Substring(0, 2) + ds.Tables[0].Rows[0]["Hora"].ToString().Substring(3, 2) + " 0240 005 020748";
             //ctrl = ctrl.Substring(0, 2);
 
+            GeneraQR(ds.Tables[0].Rows[0][1].ToString(), ds.Tables[0].Rows[0][15].ToString(), ds.Tables[0].Rows[0][0].ToString(), ds.Tables[0].Rows[0][8].ToString(), NombRep.Substring(NombRep.IndexOf(".rdlc")-1,1));
+
             if (ds.Tables[0].Rows.Count > 0)
             {
+                reportViewer1.LocalReport.EnableExternalImages = true;
+                //reportViewer1.LocalReport.
                 reportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource("dsTicket", ds.Tables[0]));
                 reportViewer1.LocalReport.ReportPath = Path.Combine(Application.StartupPath + "\\Reportes\\", NombRep);
             }
                 
+        }
+         
+        public void GeneraQR(string fecha, string cuit, string ptoVtaNComp, string total, string tipoComp)
+        {
+            string fechaJ = fecha.Substring(6) + "-" + fecha.Substring(3,2) + "-" + fecha.Substring(0,2);
+            string cuitJ = cuit.Replace("-", string.Empty);
+            string ptoVta = ptoVtaNComp.Substring(0, 4);
+            string nroComp = ptoVtaNComp.Substring(5);
+            string tipoCompJ = string.Empty;
+            string ver = "1";
+            total = total.Replace(",", ".");
+
+            if (tipoComp == "A")
+            {
+                tipoCompJ = "081";
+            }
+            else
+            {
+                tipoCompJ = "082";
+            }
+
+            string moneda = "PES";
+            string ctz = "1";
+            string tipoCodAut = "E";
+            string codAut = "70417054367476";
+            string jsonS = "ver:" + ver + ",fecha:" + fechaJ + ",cuit:" + cuitJ + ",ptoVta:" + ptoVta + ",tipoCmp:" + tipoCompJ + ",nroCmp:" + nroComp + ",importe:" + total + ",moneda:" + moneda + ",ctz:" + ctz + ",tipoCodAut:" + tipoCodAut + ",codAut:" + codAut;
+            string json64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(jsonS));
+            string url = "https://www.afip.gob.ar/fe/qr/?p=" + json64;
+
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode = new QrCode();
+
+            qrEncoder.TryEncode(url, out qrCode);
+            GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(400, QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+
+            MemoryStream ms = new MemoryStream();
+            renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+            var imageTemporal = new Bitmap(ms);
+            var imagen = new Bitmap(imageTemporal, new Size(new Point(200, 200)));
+
+            string filePath = Path.Combine(Application.StartupPath + "\\Reportes", "QR.png");
+
+            Image ImagenGrb = imagen;
+
+            ImagenGrb.Save(filePath);
         }
     }
 }
